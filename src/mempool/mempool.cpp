10bde5bd26bc124f9543c350d8e8405d94a040ff
Eugene Iegorochkin <eugene_iegorochkin@adoriasoft.com>
2020-05-15 13:30:55 +0300

    move the mempool to a separate dir as it's going to grow into multiple entities

--- src/mempool/mempool.cpp
+++ src/mempool/mempool.cpp
@@ -3,7 +3,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
-#include "veriblock/mempool.hpp"
+#include "veriblock/mempool/mempool.hpp"
 
 #include <deque>
 
