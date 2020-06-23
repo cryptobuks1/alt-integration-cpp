3f09e5c81fac9ff810469dd2601e9137a193cd04
Mr-Leshiy <leshiy12345678@gmail.com>
2020-06-23 15:47:58 +0300

    fix tests

--- test/blockchain/vbk_blockchain_test.cpp
+++ test/blockchain/vbk_blockchain_test.cpp
@@ -5,7 +5,6 @@
 
 #include <gtest/gtest.h>
 
-#include <fstream>
 #include <memory>
 
 #include "block_headers.hpp"
--- test/blockchain/vbk_blockchain_util_test.cpp
+++ test/blockchain/vbk_blockchain_util_test.cpp
@@ -9,6 +9,7 @@
 
 #include "veriblock/arith_uint256.hpp"
 #include "veriblock/blockchain/vbk_blockchain_util.hpp"
+#include "veriblock/blockchain/pop/vbk_block_tree.hpp"
 #include "veriblock/blockchain/vbk_chain_params.hpp"
 #include "veriblock/time.hpp"
 
